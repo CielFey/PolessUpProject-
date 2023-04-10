fc514406df2f19a47bfbc3460260072f2ac2732c
Arsenii <Arsenii@DESKTOP-GCS8D90>
2023-04-10 11:09:27 +0300

    Добавьте файлы проекта.

---
+++ PolessUpProject!/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PolessUpProject_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Parts = { "    +\"\"\"\"\"+ ", "   (| o o |)", "    |  ^  | ", "    | '-' | ", "    +-----+" };
+            for (int i = 0; i < Parts.Length; i++)
+            {
+                Console.WriteLine(Parts[i]);
+            }
+        }
+    }
+}
